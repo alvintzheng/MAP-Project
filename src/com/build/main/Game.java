29d4e61aa6af50f074362209ba5a6434baa78bd6
alvintzheng <alvintz@yahoo.com>
2015-12-07 18:01:40 -0800

    Update Game.java

--- src/com/build/main/Game.java
+++ src/com/build/main/Game.java
@@ -1,1 +1,18 @@
-asdf
+package com.build.main;
+
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.util.Random;
+
+public class Game implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
